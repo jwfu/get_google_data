87c67597beeeefc0e36d8b12848396219993eb4d
Jun Wei <fprimed@gmail.com>
2016-02-17 12:27:01 -0800

    fixed datetime and column names for unusual exchanges
    the function correctly parses multiday and odd timestamps (for instance on 'CURRENCY' exchange), as well as shifting column names

--- get_google_data.py
+++ get_google_data.py
@@ -3,19 +3,29 @@ import datetime as dt
 import pandas as pd
 
 def get_google_data(symbol, period, window, exch = 'NYSE'):
-    url_root = ('http://www.google.com/finance/getprices?i=' 
+    url_root = ('http://www.google.com/finance/getprices?i='
                 + str(period) + '&p=' + str(window)
                 + 'd&f=d,o,h,l,c,v&df=cpct&x=' + exch.upper() 
                 + '&q=' + symbol.upper())
     response = urllib.request.urlopen(url_root)
     data=response.read().decode().split('\n')       #decode() required for Python 3
-    data=pd.DataFrame([data[i].split(',') for i in range(len(data)-1)])
-    anchor_stamp=dt.datetime.fromtimestamp(int(data.ix[7,0][1:]))
-    df=data.drop(range(7))
-    df.index = pd.date_range(start = anchor_stamp,
-                             periods = len(data)-7, 
-                             freq = str(int(period/60)) + 'min')
-    df = df.drop(0, axis =1)
-    df.columns = ['Open', 'Hi', 'Lo', 'Close', 'Volume']
+    data = [data[i].split(',') for i in range(len(data)-1)]
+    header = data[0:7]
+    data = data[7:]
+    header[4][0] = header[4][0][8:]                           #get rid of 'Columns:' for label row
+    df=pd.DataFrame(data, columns=header[4])
     
-    return df
+    ind=pd.Series(len(df))
+    for i in range(len(df)):
+        if df['DATE'].ix[i][0] == 'a':
+            anchor_time = dt.datetime.fromtimestamp(int(df['DATE'].ix[i][1:]))  #make datetime object out of 'a' prefixed unix timecode
+            ind[i]=anchor_time
+        else:
+            ind[i] = anchor_time +dt.timedelta(seconds = (period * int(df['DATE'].ix[i])))
+    df.index = ind
+    for i in range(1,6): #shitty implementation because to_numeric is pd but does not accept df
+        df.ix[:,i]=pd.to_numeric(df.ix[:,i])
+        
+    df=df.drop('DATE', 1)
+    
+    return df, url_root
