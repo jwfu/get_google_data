bcc1bcbc62813d7819dd1c408058462032449ec7
AMAIISDOM\junwei.fu <junwei.fu@amadeus.com>
2017-04-11 12:05:21 +0100

    Minor changes to account for changes in the API return.  There are often more timeoffsets returned now in non-header rows.

--- get_google_data.py
+++ get_google_data.py
@@ -5,16 +5,18 @@ import pandas as pd
 def get_google_data(symbol, period, window, exch = 'NYSE'):
     url_root = ('http://www.google.com/finance/getprices?i='
                 + str(period) + '&p=' + str(window)
-                + 'd&f=d,o,h,l,c,v&df=cpct&x=' + exch.upper() 
+                + 'd&f=d,o,h,l,c,v&df=cpct&x=' + exch.upper()
                 + '&q=' + symbol.upper())
     response = urllib.request.urlopen(url_root)
     data=response.read().decode().split('\n')       #decode() required for Python 3
     data = [data[i].split(',') for i in range(len(data)-1)]
     header = data[0:7]
     data = data[7:]
-    header[4][0] = header[4][0][8:]                           #get rid of 'Columns:' for label row
+    header[4][0] = header[4][0][8:]                 #get rid of 'Columns:' for label row
     df=pd.DataFrame(data, columns=header[4])
-    
+    df = df.dropna()                                #to fix the inclusion of more timezone shifts in the .csv returned from the goog api
+    df.index = range(len(df))                       #fix the index from the previous dropna()
+
     ind=pd.Series(len(df))
     for i in range(len(df)):
         if df['DATE'].ix[i][0] == 'a':
@@ -23,10 +25,10 @@ def get_google_data(symbol, period, window, exch = 'NYSE'):
         else:
             ind[i] = anchor_time +dt.timedelta(seconds = (period * int(df['DATE'].ix[i])))
     df.index = ind
-        
+
     df=df.drop('DATE', 1)
 
     for column in df.columns:                #shitty implementation because to_numeric is pd but does not accept df
         df[column]=pd.to_numeric(df[column])
-    
+
     return df
